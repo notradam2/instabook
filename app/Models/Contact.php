d0350377ec591228f41a0ee39bfef14f322c2a9b
notradam2 <narciso.notra@fullspeedtechnologies.com>
2022-09-20 10:30:43 +0800

    Laravel installation.

---
+++ app/Models/Contact.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Models;
+
+use App\Services\AvailableSlot\SlotCollection;
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Contact extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * Acceptable menu type constant variables
+     */
+    public const ACCEPTABLE_MENU_TYPE_ALL = 1;
+    public const ACCEPTABLE_MENU_TYPE_SELECTED = 2;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'contacts';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'phone',
+        'photo',
+        'birthdate',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'birthdate' => 'date',
+    ];
+
+    /**
+     * Get the User associated with the contact.
+     *
+     * @return BelongsTo
+     *
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
